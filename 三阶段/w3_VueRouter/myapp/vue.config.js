33e924fcdb7782958cd53144fa626418b8c625df
aaron <jeroo@qq.com>
2021-05-15 17:24:59 +0800

    day3-5 项目要求

--- 三阶段/w3_VueRouter/myapp/vue.config.js
+++ 三阶段/w3_VueRouter/myapp/vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    lintOnSave:'warning'
+    lintOnSave:'warning',
+    devServer:{
+        proxy:{
+            '/api':{
+                // 目标服务器
+                target: 'https://10.3.133.6:2101', 
+                changeOrigin: true,
+                pathRewrite:{
+                    '^/api/':'/api'
+                },
+            },
+            '/jd':{
+                target: 'https://www.jd.com', 
+            }
+            
+        }
+    }
 }
