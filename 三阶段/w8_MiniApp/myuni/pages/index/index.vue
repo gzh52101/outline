d19c4b10fd792b6ebe630e0b135d623227b23a3a
aaron <jeroo@qq.com>
2021-06-21 18:17:34 +0800

    day9-1 小程序跨平台框架

---
+++ 三阶段/w8_MiniApp/myuni/pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<!-- index.wxml -->
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="msg">msg</view>
+		<button @click="goto('/pages/mine/mine')">Mine</button>
+	</view>
+</template>
+
+<script>
+	// index.js
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.title = 'hello uni'
+		},
+		methods: {
+			getData(){
+				// uni.request()	-> wx.request
+				// 				-> 
+			},
+			goto(path){
+				uni.navigateTo({
+					url:path
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	/*index.wxss*/ 
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #f00;
+	}
+</style>
---
+++ 三阶段/w8_MiniApp/myuni/pages/mine/mine.vue
@@ -0,0 +1,58 @@
+<template>
+	<!-- index.wxml -->
+	<view class="content">
+		<h1>我的</h1>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	// index.js
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.title = 'hello uni'
+		},
+		methods: {
+			getData(){
+				// uni.request()	-> wx.request
+				// 				-> 
+			}
+		}
+	}
+</script>
+
+<style>
+	/*index.wxss*/ 
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #f00;
+	}
+</style>
