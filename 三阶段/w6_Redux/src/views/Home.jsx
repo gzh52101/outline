84c84bee30e82e9ced52ede345221baf9817425d
aaron <jeroo@qq.com>
2021-06-24 10:41:03 +0800

    day9-4 app打包

--- 三阶段/w6_Redux/src/views/Home.jsx
+++ 三阶段/w6_Redux/src/views/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useCallback} from 'react'
 
 import { withStorage } from '@/utils/hoc'
 import request from '@/utils/request';
@@ -7,12 +7,21 @@ function Home(props) {
     request.get('/class',{size:20,page:1,category: "HTML5"}).then(res=>{
         
     })
+    const scan = useCallback(()=>{
+        try{
+            plus.barcode.create()
+        }catch(err){
+            console.log('请在打包后使用该功能')
+        }
+    },[])
     return (
         <div>
             Home
             <button onClick={() => {
                 props.history.push('/login')
             }}>去登录</button>
+
+            <button onClick={scan}>扫码</button>
         </div>
     )
 }
